c5b2ab8f390bd5bdbe2ace73e30f7b2f4064a9a1
katie <katiequin335@gmail.com>
2015-06-04 19:23:25 -0400

    Final commit

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
-$(document).ready(function() {
- for(var i = 1; i <= 16; i++) {
-  $('.content').append('<div>TESTING</div>');
- }
+$(document).ready(function () {
+    for (var i = 0; i < 247; i++) {
+        $('.content').append('<div class="square"></div>');
+    }
+    var color = "black";
+    $('.square').mouseenter(function () {
+        $(this).css("background-color", color);
+    });
+    $('button').click(function () {
+        $('.square').css("background-color", "white");
+    });
+    $('#red').click(function () {
+        color = "red";
+    });
+    $('#orange').click(function () {
+        color = "orange";
+    });
+     $('#yellow').click(function () {
+        color = "yellow";
+    });
+    $('#green').click(function () {
+        color = "green";
+    });
+     $('#blue').click(function () {
+        color = "blue";
+    });
+     $('#purple').click(function () {
+        color = "purple";
+    });
+     $('#black').click(function () {
+        color = "black";
+    });
+    $('#white').click(function () {
+        color = "white";
+    });
+
 });
